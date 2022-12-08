ad8d66d0d30aec773a44b28270466c444795deef
jose1678 <alfredosalazarg@yahoo.com.ar>
2022-12-08 15:23:46 -0300

    'practica2'

--- 02.js
+++ 02.js
@@ -11,7 +11,23 @@ function stringMasLarga(strings) {
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
-  // Tu código aca
+  // Tu código aca result = array.reduce((a, b) => a.length > b.length ? a : b);
+var masLarga = "";
+for(var i = 0; i < strings.length; i++){
+  if(strings[i].length > masLarga.length) {
+    masLarga = strings[i];
+  }
+
+}
+return masLarga;
+  
+  var stringMasLarga = "";
+  for(var i = 0; i < strings.length; i++){
+    const element = String[i];
+    if(stringMasLarga.length < element.length) stringMasLarga = element
+  }
+  return stringMasLarga;
+
 }
 
 // No modifiques nada debajo de esta linea //
